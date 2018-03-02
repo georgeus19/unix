3d3e740421efae04a7a2ae4ac4389c7d012149af
Krystof Hruby <hrubykr@u-pl0.ms.mff.cuni.cz>
2018-03-02 12:27:30 +0100

    Druhy ukol

---
+++ u02/program.pas
@@ -0,0 +1,13 @@
+begin
+writeln('ahoj');
+Writeln('12123123');
+writeln('     ');
+Writeln('dsada1123  ');
+Writeln('ada sdas');
+Writeln('dasdasda');
+Writeln('32fddf43');
+Writeln('322fds    ');
+Writeln('dsfdsko');
+Writeln('3249023');
+Writeln('0000  fdf 00');
+end.
